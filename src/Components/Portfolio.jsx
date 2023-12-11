8c844a0c0a5ccfb4d9e71b6ed5da407baa1f5545
emmjje <119668591+emmjje@users.noreply.github.com>
2023-12-11 14:40:48 +0700

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -32,7 +32,7 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "Crafting & Developing Technuvibe",
+    title: "Building Technuvibe",
     description:
       "Explore a world of possibilities with our innovative job portal. We are dedicated to connecting you with rewarding career opportunities that match your skills and aspirations..",
     url: "https://www.technuvibe.com/",
